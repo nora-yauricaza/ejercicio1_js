f3b04aa61de8b7f35a08c68525fbaf9233e2034a
nora-yauricaza <nora-yauricaza>
2016-08-25 14:37:52 -0500

    ejercicio1

---
+++ ejercicio.js
@@ -0,0 +1,6 @@
+  var n1 = prompt("Ingresa primer numero");
+  var n2 = prompt("Ingresa segundo numero");
+  document.write("La suma de "+ n1 + " y "+ n2 + " es: ");
+  document.write(parseInt(n1)+parseInt(n2));
+
+  
